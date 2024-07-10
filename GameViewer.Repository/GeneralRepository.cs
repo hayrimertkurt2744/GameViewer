b7c0de9f7b8e2d6327b5fc21820155371a7c2bc6
Hayri Mert kurt <hayrimertkurt@outlook.com>
2024-07-10 21:20:44 +0300

    General repository was set as a generic type.

---
+++ GameViewer.Repository/GeneralRepository.cs
@@ -0,0 +1,60 @@
+﻿using GameViewer.Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameViewer.Repository
+{
+    internal class GeneralRepository
+    {
+        private readonly GameViewerContext _context;
+        public GeneralRepository()
+        {
+            _context=new GameViewerContext();
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            using (var context = _context)
+            {
+                context.Set<T>().Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            using (var context = _context)
+            {
+                context.Set<T>().Update(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            using (var context = _context)
+            {
+                context.Set<T>().Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public T GetById<T>(int id) where T : class
+        {
+            using (var context = _context)
+            {
+                return context.Set<T>().Find(id);
+            }
+        }
+
+        public List<T> GetAll<T>() where T : class
+        {
+            using (var context = _context)
+            {
+                return context.Set<T>().ToList();
+            }
+        }
+    }
+}
