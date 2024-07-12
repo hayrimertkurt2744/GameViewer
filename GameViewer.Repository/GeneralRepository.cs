6733fc6d773f1dbbadc6d80441f9e6e21850908c
Hayri Mert kurt <hayrimertkurt@outlook.com>
2024-07-12 23:33:42 +0300

    +Get repo methods were added.

--- GameViewer.Repository/GeneralRepository.cs
+++ GameViewer.Repository/GeneralRepository.cs
@@ -3,7 +3,9 @@ using GameViewer.Dal.Entities;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -40,10 +42,74 @@ namespace GameViewer.Repository
         {
             return _context.ProviderParams.ToList();
         }
-        public List<ParamValue> GetParamValues()
+        public DataTable GetAllProviderParams()
         {
-            return _context.ParamValues.ToList();
+            var providerParams = _context.ProviderParams.ToList();
+
+            var dataTable = new DataTable();
+            dataTable.Columns.Add("Id", typeof(int));
+            dataTable.Columns.Add("Name", typeof(string));
+            dataTable.Columns.Add("ProviderId", typeof(string));
+            dataTable.Columns.Add("Features", typeof(string));
+            dataTable.Columns.Add("OrderId", typeof(int));
+            dataTable.Columns.Add("Type", typeof(string));
+            dataTable.Columns.Add("DefaultValue", typeof(string));
+
+            foreach (var providerParam in providerParams)
+            {
+                var row = dataTable.NewRow();
+                row["Id"] = providerParam.Id;
+                row["ProviderId"] = providerParam.ProviderId;
+                row["Name"] = providerParam.Name; 
+                row["Features"] = providerParam.Features;
+                row["OrderId"] = providerParam.OrderId;
+                row["Type"] = providerParam.Type;
+                row["DefaultValue"] = providerParam.DefaultValue;
+
+                dataTable.Rows.Add(row);
+            }
+
+            return dataTable;
         }
+        //public DataTable GetPageValuesById(int pageId)
+        //{
+        //    var Parameters = _context.ProviderParams
+        //        .Where(cp => cp.ProviderId == pageId)
+        //        .OrderBy(cp => cp.OrderId)
+        //        .ToList();
+
+        //    var ParameterValues = _context.ParamValues
+        //        .Where(cpv => Parameters.Select(cp => cp.Id).Contains(cpv.ParameterId))
+        //        .ToList();
+
+        //    var dataTable = new DataTable();
+
+        //    dataTable.Columns.Add("Id", typeof(int));
+        //    foreach (var param in Parameters)
+        //    {
+        //        dataTable.Columns.Add(param.ColumnName, typeof(string));
+        //    }
+
+        //    var groupedValues = ParameterValues
+        //        .GroupBy(cpv => cpv.RowId)
+        //        .ToDictionary(g => g.Key, g => g.ToList());
+
+        //    foreach (var group in groupedValues)
+        //    {
+        //        var row = dataTable.NewRow();
+        //        row["Id"] = group.Key;
+
+        //        foreach (var param in Parameters)
+        //        {
+        //            var value = group.Value.FirstOrDefault(v => v.ParameterId == param.Id)?.Value;
+        //            row[param.ColumnName] = value ?? param.DefaultValue;
+        //        }
+
+        //        dataTable.Rows.Add(row);
+        //    }
+
+        //    return dataTable;
+        //}
 
     }
 }
